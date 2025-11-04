da50e4bf9ed007dd2a8e1948bf3ba81e5048005a
LasyaYadlapati <syadlapati@ucsd.edu>
2025-11-04 13:39:59 -0800

    upload sweep running script

---
+++ run_sweep.py
@@ -0,0 +1,10 @@
+import wandb
+import yaml
+
+if __name__ == "__main__":
+    yaml_file_path = 'scripts/sweep.yaml'
+    with open(yaml_file_path, 'r') as file:
+        sweep_configuration = yaml.safe_load(file)
+
+    sweep_id = wandb.sweep(sweep=sweep_configuration, project="phoneme-sweeps")
+    wandb.agent(sweep_id)
